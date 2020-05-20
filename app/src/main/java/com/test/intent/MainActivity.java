efd5f2d0a23acdce05ed5cb906745292472d62d2
AlexeyMichkov <63221821+AlexeyMichkov@users.noreply.github.com>
2020-05-20 16:57:19 +0500

    Передача данных между Activity

--- app/src/main/java/com/test/intent/MainActivity.java
+++ app/src/main/java/com/test/intent/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    public void onClick(View view) {
+    public void onClick(View v) {
+
+        final EditText nameText = findViewById(R.id.name);
+        final EditText companyText = findViewById(R.id.company);
+        final EditText priceText = findViewById(R.id.price);
+
+        String name = nameText.getText().toString();
+        String company = companyText.getText().toString();
+        int price = Integer.parseInt(priceText.getText().toString());
+
         Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("company", company);
+        intent.putExtra("price", price);
         startActivity(intent);
     }
 }
+
+/*В обработчике нажатия кнопки получаем введенные в текстовые поля EditText данные и передаем их в объект Intent с помощью метода putExtra().
+Затем запускаем SecondActivity.
+В итоге при нажатии на кнопку запустится SecondActivity, которая получит некоторые введенные в текстовые поля данные.
+* */
--- app/src/main/java/com/test/intent/SecondActivity.java
+++ app/src/main/java/com/test/intent/SecondActivity.java
@@ -13,7 +13,20 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = new TextView(this);
         textView.setTextSize(20);
         textView.setPadding(16, 16, 16, 16);
-        textView.setText("Лёха, красавчег!");
+
+        Bundle arguments = getIntent().getExtras();
+
+        if(arguments!=null){
+            String name = arguments.get("name").toString();
+            String company = arguments.getString("company");
+            int price = arguments.getInt("price");
+            textView.setText("Name: " + name + "\nCompany: " + company +
+                    "\nPrice: " + price);
+        }
+
         setContentView(textView);
     }
 }
+
+/*В данном случае в SecondActivity получаем все данных из объекта Bundle и выводим их в текстовое поле TextView.
+Предполагается, что данной activity будут передаваться три элемента - две строки с ключами name и company и число с ключом price.*/
